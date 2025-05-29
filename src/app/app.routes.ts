64e8e0bdb54ecaf85df29d278d4ce7260ee20090
Martin Trujillo <martin.trujillo@uni.pe>
2025-05-29 10:06:39 -0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 
-import { DashboardComponent } from './admin/dashboard.component';
 import { PrincipalComponent } from './public/principal.component';
+import { AdminPrincipalComponent } from './admin/admin-principal.component';
 
 export const routes: Routes = [
   { path: '', component: PrincipalComponent },
-  { path: 'admin', component: DashboardComponent },
+  { path: 'admin', component: AdminPrincipalComponent },
   { path: '**', redirectTo: '' } // Ruta por defecto si no existe  
 ];
